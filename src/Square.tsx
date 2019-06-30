35c6cbae00cf0c668ccad34435dba9b0750bc36a
TSUYUKI kohei <kotsuyuki@gmail.com>
2019-06-30 15:33:08 +0900

    create interactive component

--- src/Square.tsx
+++ src/Square.tsx
@@ -1,10 +1,24 @@
 import * as React from 'react'
 
 export default class Square extends React.Component<{value: number}> {
+    state = {
+        value: null
+    }
+
+    constructor(props: {value: number}) {
+        super(props)
+    }
+
+    squareOnClicked = () => {
+        this.setState({
+            value: 'X'
+        })
+    }
+
     render() {
         return (
-        <button className="square">
-            {this.props.value}
+        <button className="square" onClick={this.squareOnClicked}>
+            {this.state.value}
         </button>
         );
     }
